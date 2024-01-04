8ed17d1a3ce3063a34d1bd29a7902d403c7e07d6
awsandy <awsandy@amazon.co.uk>
2024-01-04 13:20:19 +0000

    up

--- 01-zero-etl/12.sh
+++ 01-zero-etl/12.sh
@@ -1,4 +1,4 @@
-intid=$(aws rds describe-integrations --query 'Integrations[].IntegrationArn' --output text | rev | cut -f1 -d':')
+intid=$(aws rds describe-integrations --query 'Integrations[].IntegrationArn' --output text | rev | cut -f1 -d':' | rev) 
 cat << EOF > 12-create-db-from-integration.sql
 CREATE DATABASE aurora_zeroetl FROM INTEGRATION ${intid};
 EOF
