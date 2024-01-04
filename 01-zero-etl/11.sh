0c563387377ee3238417c38d0918be0c522bae80
awsandy <awsandy@amazon.co.uk>
2024-01-04 13:08:55 +0000

    up

--- 01-zero-etl/11.sh
+++ 01-zero-etl/11.sh
@@ -22,5 +22,5 @@
 export ENDPOINT=$(aws redshift-serverless  list-workgroups --query 'workgroups[].endpoint.address' --output text | grep zero-etl-destination)
 export PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id "RedshiftServerlessSecret" --query SecretString --output text | jq -r .password)
 export PGUSER=$(aws secretsmanager get-secret-value --secret-id "RedshiftServerlessSecret" --query SecretString --output text | jq -r .username)
-psql dev -h $ENDPOINT -U $PGUSER -p 5439 -f ./01-zero-etl/11-query-integration.sql
+psql dev -h $ENDPOINT -U $PGUSER -p 5439 -f ./11-query-integration.sql
 
