98ca942a2315bfcb7b03516f9f4c208806f4ed51
awsandy <awsandy@amazon.co.uk>
2024-01-04 13:17:20 +0000

    up

--- 01-zero-etl/12.sh
+++ 01-zero-etl/12.sh
@@ -2,3 +2,7 @@ intid=$(aws rds describe-integrations --query 'Integrations[].IntegrationArn' --
 cat << EOF > 12-create-db-from-integration.sql
 CREATE DATABASE aurora_zeroetl FROM INTEGRATION ${intid};
 EOF
+export ENDPOINT=$(aws redshift-serverless  list-workgroups --query 'workgroups[].endpoint.address' --output text | grep zero-etl-destination)
+export PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id "RedshiftServerlessSecret" --query SecretString --output text | jq -r .password)
+export PGUSER=$(aws secretsmanager get-secret-value --secret-id "RedshiftServerlessSecret" --query SecretString --output text | jq -r .username)
+psql dev -h $ENDPOINT -U $PGUSER -p 5439 -f ./12-create-db-from-integration.sql
