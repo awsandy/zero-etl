bede1dcc8af0058a2b6774846d19cc308d8511ce
awsandy <awsandy@amazon.co.uk>
2024-01-04 13:07:38 +0000

    up

--- 01-zero-etl/11.sh
+++ 01-zero-etl/11.sh
@@ -6,20 +6,20 @@
 #    {\"name\": \"param5\", \"value\": \"${PARAM5}\"}, \
 #    {\"name\": \"param6\", \"value\": \"${PARAM6}\"}\
 #    ]"
-CLUSTER_IDENTIFIER="zero-etl-destination-dw-wg-db265cc0.008088116876.us-west-2.redshift-serverless.amazonaws.com:5439/dev"
-ENDPOINT=$(aws redshift-serverless  list-workgroups --query 'workgroups[].endpoint.address' --output text | grep zero-etl-destination)
+#CLUSTER_IDENTIFIER="zero-etl-destination-dw-wg-db265cc0.008088116876.us-west-2.redshift-serverless.amazonaws.com:5439/dev"
 #CREDENTIALS_ARN=$(aws secretsmanager get-secret-value --secret-id "RedshiftServerlessSecret" --query SecretString --output text)
-CREDENTIALS_ARN=$(aws secretsmanager list-secrets --query 'SecretList[].ARN' | grep RedshiftServerless | tr -d ' |,|"')
-SCRIPT_SQL=$(tr -d '\n' <./11-query-integration.sql)
+#CREDENTIALS_ARN=$(aws secretsmanager list-secrets --query 'SecretList[].ARN' | grep RedshiftServerless | tr -d ' |,|"')
+#SCRIPT_SQL=$(tr -d '\n' <./11-query-integration.sql)
 
-AWS_RESPONSE=$(aws redshift-data execute-statement \
-    --cluster-identifier $CLUSTER_IDENTIFIER \
-    --sql "$SCRIPT_SQL" \
-    --region us-west-2 \
-    --database dev \
-    --secret $CREDENTIALS_ARN)
+#AWS_RESPONSE=$(aws redshift-data execute-statement \
+#    --cluster-identifier $CLUSTER_IDENTIFIER \
+#    --sql "$SCRIPT_SQL" \
+#    --region us-west-2 \
+#    --database dev \
+#    --secret $CREDENTIALS_ARN)
 
-# on EC2 instance
+# on EC2 instance - plsql for redshift
+export ENDPOINT=$(aws redshift-serverless  list-workgroups --query 'workgroups[].endpoint.address' --output text | grep zero-etl-destination)
 export PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id "RedshiftServerlessSecret" --query SecretString --output text | jq -r .password)
 export PGUSER=$(aws secretsmanager get-secret-value --secret-id "RedshiftServerlessSecret" --query SecretString --output text | jq -r .username)
 psql dev -h $ENDPOINT -U $PGUSER -p 5439 -f ./01-zero-etl/11-query-integration.sql
