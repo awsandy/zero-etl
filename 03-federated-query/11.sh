08f5bd829b27d27825185e12546e24c20e20d511
awsandy <awsandy@amazon.co.uk>
2024-01-05 13:28:30 +0000

    up

--- 03-federated-query/11.sh
+++ 03-federated-query/11.sh
@@ -1,9 +1,9 @@
-#export ENDPOINT=$(aws redshift-serverless  list-workgroups --query 'workgroups[].endpoint.address' --output text | grep zero-etl-destination)
-#export PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id "RedshiftServerlessSecret" --query SecretString --output text | jq -r .password)
-#export PGUSER=$(aws secretsmanager get-secret-value --secret-id "RedshiftServerlessSecret" --query SecretString --output text | jq -r .username)
-#psql dev -h $ENDPOINT -U $PGUSER -p 5439 -f $fn
+export ENDPOINT=$(aws redshift-serverless  list-workgroups --query 'workgroups[].endpoint.address' --output text | grep zero-etl-destination)
+export PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id "RedshiftServerlessSecret" --query SecretString --output text | jq -r .password)
+export PGUSER=$(aws secretsmanager get-secret-value --secret-id "RedshiftServerlessSecret" --query SecretString --output text | jq -r .username)
 me=$(basename "$0")
 fn=$(echo $me | cut -d '.' -f 1)
-echo $fn
-find . -name $fn*.sql
+sfln=$(find . -name $fn*.sql)
+echo $sfln
+#psql dev -h $ENDPOINT -U $PGUSER -p 5439 -f $sfln
 
