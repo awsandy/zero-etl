e59e72dcd42ece60d40cde04ceffccb55b7a69e2
awsandy <awsandy@amazon.co.uk>
2024-01-05 15:29:54 +0000

    up

--- 03-federated-query/21.sh
+++ 03-federated-query/21.sh
@@ -11,4 +11,4 @@ EOF
 export ENDPOINT=$(aws redshift-serverless  list-workgroups --query 'workgroups[].endpoint.address' --output text | grep zero-etl-destination)
 export PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id "RedshiftServerlessSecret" --query SecretString --output text | jq -r .password)
 export PGUSER=$(aws secretsmanager get-secret-value --secret-id "RedshiftServerlessSecret" --query SecretString --output text | jq -r .username)
-#psql dev -h $ENDPOINT -U $PGUSER -p 5439 -f ./21-cre-ext-schema.sql
+psql dev -h $ENDPOINT -U $PGUSER -p 5439 -f ./21-cre-ext-schema.sql
