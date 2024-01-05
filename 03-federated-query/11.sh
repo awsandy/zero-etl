f06179d1aee57aa5a90920839373a15a487b27e7
awsandy <awsandy@amazon.co.uk>
2024-01-05 13:26:49 +0000

    up

--- 03-federated-query/11.sh
+++ 03-federated-query/11.sh
@@ -3,6 +3,7 @@
 #export PGUSER=$(aws secretsmanager get-secret-value --secret-id "RedshiftServerlessSecret" --query SecretString --output text | jq -r .username)
 #psql dev -h $ENDPOINT -U $PGUSER -p 5439 -f $fn
 me=$(basename "$0")
-echo $me
 fn=$(echo $me | cut -d '.' -f 1)
 echo $fn
+find . -name $fn*.sql
+
