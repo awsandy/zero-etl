5552381abe59479d6619e73c18cd86848fdc5216
awsandy <awsandy@amazon.co.uk>
2024-01-06 10:29:42 +0000

    up

---
+++ 04-analytics-ml/13.sh
@@ -0,0 +1,10 @@
+export ENDPOINT=$(aws redshift-serverless  list-workgroups --query 'workgroups[].endpoint.address' --output text | grep zero-etl-destination)
+export PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id "RedshiftServerlessSecret" --query SecretString --output text | jq -r .password)
+export PGUSER=$(aws secretsmanager get-secret-value --secret-id "RedshiftServerlessSecret" --query SecretString --output text | jq -r .username)
+me=$(basename "$0")
+fn=$(echo $me | cut -d '.' -f 1)
+sfln=$(find . -name $fn*.sql)
+echo "running $sfln"
+psql dev -h $ENDPOINT -U $PGUSER -p 5439 -f $sfln
+echo "done"
+
